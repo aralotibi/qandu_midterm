df2cb57d0e79d26e26b7b368684a97a382f67dce
Aralotibi <aralotibi@quinnipiac.edu>
2015-10-22 02:35:36 +0000

    Add Message to admin panel

--- core/models.py
+++ core/models.py
@@ -6,3 +6,6 @@ class Message(models.Model):
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
+  def __unicode__(self):
+      return self.title
+
